684f6f6efdea17ef3518ad3dd59deb95ab01215f
S_A_T <avtobot.08@gmail.com>
2014-12-06 00:20:57 +0200

    Starting

---
+++ LZW/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace LZW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openImage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openImage
+            // 
+            this.openImage.Location = new System.Drawing.Point(12, 12);
+            this.openImage.Name = "openImage";
+            this.openImage.Size = new System.Drawing.Size(75, 23);
+            this.openImage.TabIndex = 0;
+            this.openImage.Text = "Открыть";
+            this.openImage.UseVisualStyleBackColor = true;
+            this.openImage.Click += new System.EventHandler(this.openImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 262);
+            this.Controls.Add(this.openImage);
+            this.Name = "Form1";
+            this.Text = "LZW main © Artem Shevchenko";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openImage;
+    }
+}
+
