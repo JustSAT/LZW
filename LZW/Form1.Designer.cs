c90f0de3b388f9cd7055e40ef5d1efd92c099b32
S_A_T <avtobot.08@gmail.com>
2014-12-06 16:02:41 +0200

    Dev 1

--- LZW/Form1.Designer.cs
+++ LZW/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.openImage = new System.Windows.Forms.Button();
+            this.compress = new System.Windows.Forms.Button();
+            this.decompress = new System.Windows.Forms.Button();
+            this.openCopressed = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // openImage
@@ -41,12 +44,48 @@
             this.openImage.UseVisualStyleBackColor = true;
             this.openImage.Click += new System.EventHandler(this.openImage_Click);
             // 
+            // compress
+            // 
+            this.compress.Location = new System.Drawing.Point(93, 12);
+            this.compress.Name = "compress";
+            this.compress.Size = new System.Drawing.Size(75, 23);
+            this.compress.TabIndex = 1;
+            this.compress.Text = "Сжать";
+            this.compress.UseVisualStyleBackColor = true;
+            this.compress.Click += new System.EventHandler(this.compress_Click);
+            // 
+            // decompress
+            // 
+            this.decompress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.decompress.Location = new System.Drawing.Point(176, 12);
+            this.decompress.Name = "decompress";
+            this.decompress.Size = new System.Drawing.Size(75, 23);
+            this.decompress.TabIndex = 2;
+            this.decompress.Text = "Расжать";
+            this.decompress.UseVisualStyleBackColor = true;
+            this.decompress.Click += new System.EventHandler(this.decompress_Click);
+            // 
+            // openCopressed
+            // 
+            this.openCopressed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.openCopressed.Location = new System.Drawing.Point(257, 12);
+            this.openCopressed.Name = "openCopressed";
+            this.openCopressed.Size = new System.Drawing.Size(75, 23);
+            this.openCopressed.TabIndex = 3;
+            this.openCopressed.Text = "Открыть";
+            this.openCopressed.UseVisualStyleBackColor = true;
+            this.openCopressed.Click += new System.EventHandler(this.openCopressed_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(472, 262);
+            this.ClientSize = new System.Drawing.Size(344, 102);
+            this.Controls.Add(this.openCopressed);
+            this.Controls.Add(this.decompress);
+            this.Controls.Add(this.compress);
             this.Controls.Add(this.openImage);
+            this.MinimumSize = new System.Drawing.Size(360, 140);
             this.Name = "Form1";
             this.Text = "LZW main © Artem Shevchenko";
             this.ResumeLayout(false);
@@ -56,6 +95,9 @@
         #endregion
 
         private System.Windows.Forms.Button openImage;
+        private System.Windows.Forms.Button compress;
+        private System.Windows.Forms.Button decompress;
+        private System.Windows.Forms.Button openCopressed;
     }
 }
 
--- LZW/Form1.cs
+++ LZW/Form1.cs
@@ -12,6 +12,12 @@ namespace LZW
 {
     public partial class Form1 : Form
     {
+        public Bitmap openedImage = null;
+        public byte[] imageBytes = null;
+        private int[] table;
+
+        private Stream openedCompressed = null;
+        private string filePath = "";
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +25,138 @@ namespace LZW
 
         private void openImage_Click(object sender, EventArgs e)
         {
-           
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "gif files (*.gif)|*.gif";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            openedImage = new Bitmap(Image.FromStream(myStream));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void compress_Click(object sender, EventArgs e)
+        {
+            MakeTable();
+        }
+
+        private void MakeTable()
+        {
+            StreamWriter outStream = new StreamWriter("C:/out.txt");
+            imageBytes = imageToByteArray(openedImage);
+            table = new int[openedImage.Width * openedImage.Height];
+            for (int i = 0; i < openedImage.Width * openedImage.Height; i++)
+            {
+                if (i <= 255)
+                    table[i] = i;
+                else
+                    table[i] = -1;
+            }
+            
+
+            int curByte = imageBytes[0];
+            string str = "" + (char)imageBytes[0];
+            int id = 0;
+            for (int i = 1; i < imageBytes.Length; i++)
+            {
+                char ch = (char)imageBytes[i];
+                id = 0;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    id += str[j];
+                }
+                id += ch;
+                if (table[id] != -1)
+                {
+                    str += ch;
+                }
+                else
+                {
+                    //out
+                    outStream.Write(str);
+                    table[id] = id + ch;
+                    str = "" + ch;
+                }
+            }
+            //out
+            outStream.Write(str);
+            outStream.Close();
+            
+        }
+
+        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+        }
+
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+        
+        private void openCopressed_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            openedCompressed = myStream;
+                            filePath = openFileDialog1.FileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void decompress_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            StreamWriter write = new StreamWriter("c:/test.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if(i != lines.Length-1)
+                write.WriteLine(lines[i]);
+                else
+                    write.Write(lines[i]);
+            }
+            write.Close();
         }
     }
 }
